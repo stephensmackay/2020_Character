27c2359a488a554951993e4122872183e5b92d0e
Stephen <10559104@my.uvu.edu>
2020-01-23 12:47:23 -0700

    animbehavior

---
+++ Scripting/Scripting/Assets/DebugObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class DebugObject : ScriptableObject
+{
+
+    public void OnDebug(string message)
+    {
+        Debug.Log(message);
+    }
+}
