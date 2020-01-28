99addb1465b35487ce9c490b903d217021827f4d
Stephen <10559104@my.uvu.edu>
2020-01-28 08:56:03 -0700

    Import Script Library
    Imported library from previous scripts

---
+++ Scripting/Scripting/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public interface IRun
+{
+    float Speed { get; set; }
+    void Run();
+    void Run(float f);
+}
+
+public interface ITest
+{
+    void Test();
+}
+
+public interface IListen
+{
+    UnityEvent Event { get; set; } 
+    IRun NewIRunObj { get; set; } 
+    void Start();
+}
+
+public interface ITrigger
+{
+    void OnTriggerEnter(Collider obj);
+}
+
+public interface IMove
+{
+    Vector3 location { get; set; }
+    void Move(CharacterController characterController);
+}
