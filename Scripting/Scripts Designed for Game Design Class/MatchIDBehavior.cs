9ab49116d61311c3a5b6b03a32566d56429b9846
StephenSMackay <10559104@my.uvu.edu>
2020-02-19 21:18:14 -0700

    Copy over 2
    Copying over updated scripts being worked on in Game Design

---
+++ Scripting/Scripts Designed for Game Design Class/MatchIDBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchIDBehavior : IDBehavior
+{
+    [Serializable]
+    public struct possibleMatches
+    {
+        public NameID nameIDObj;
+        public DoWork DoWorkObj;
+        
+    }
+
+    public List<possibleMatches> workIDList;
+}
---
+++ Scripting/Scripts Designed for Game Design Class/MathFunctions.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MathFunctions : DoWork
+{
+    public FloatData floatDataObj;
+    
+    
+    public override void Work()
+    {
+        floatDataObj.value++;
+        workEvent.Invoke();
+    }
+    
+}
